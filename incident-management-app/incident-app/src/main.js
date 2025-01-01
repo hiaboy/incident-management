e3d9c265ea823ae184065446620d1c8191796789
hiaboy <1161720942@qq.com>
2025-01-01 14:50:47 +0800

    操作说明提交

--- incident-management-app/incident-app/src/main.js
+++ incident-management-app/incident-app/src/main.js
@@ -5,9 +5,8 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8081', // 你的 API 基础 URL
+    baseURL: 'http://localhost:8081',
     timeout: 10000, // 请求超时时间
-    // 你可以在这里添加更多的默认设置
 });
 
 const app = createApp(App);
